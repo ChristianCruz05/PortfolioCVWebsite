74856062afa58cd2419bac1fb26d24378d374b43
Ichan05 <mcrcruz15@gmail.com>
2022-04-27 22:39:01 +0800

    first commit

---
+++ js/animations.js
@@ -0,0 +1,10 @@
+$(function(){
+    $("#neon-button").toggle();
+    $("#neon-button").fadeIn(5);
+    $("#neon-button").fadeOut(50);
+    $("#neon-button").fadeIn(10);
+    $("#neon-button").fadeOut(100);
+    $("#neon-button").fadeIn(50);
+    $("#neon-button").fadeOut(250);
+    $("#neon-button").fadeIn(1000);
+})
---
+++ js/interactions.js
@@ -0,0 +1,10 @@
+let nav = $("nav");
+$(function(){
+  window.addEventListener('scroll', function(){
+    if(window.pageYOffset>100){
+      nav.addClass('bg-black shadow')
+    }else{
+      nav.removeClass('bg-black shadow')
+    }
+  })
+})
