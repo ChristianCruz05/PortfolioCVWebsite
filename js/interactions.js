844cc5c3ccb4b85d70a373c2937e272dc04b615d
Ichan05 <mcrcruz15@gmail.com>
2022-05-11 11:41:32 +0800

    added gallery

--- js/interactions.js
+++ js/interactions.js
@@ -1,4 +1,6 @@
 let nav = $("nav");
+
+
 $(function(){
   window.addEventListener('scroll', function(){
     if(window.pageYOffset>100){
@@ -7,6 +9,22 @@ $(function(){
       nav.removeClass('bg-black shadow')
     }
   })
+
   
+  $("#about-btn").click(function(){
+    $("#about-me").removeClass("d-none")
+    $("#education").addClass("d-none");
+    $("#skills").addClass("d-none");
+  });
+  $("#education-btn").click(function(){
+    $("#education").removeClass("d-none")
+    $("#about-me").addClass("d-none");
+    $("#skills").addClass("d-none");
+  });
+  $("#skills-btn").click(function(){
+    $("#skills").removeClass("d-none")
+    $("#education").addClass("d-none");
+    $("#about-me").addClass("d-none");
+  });  
 })
 
